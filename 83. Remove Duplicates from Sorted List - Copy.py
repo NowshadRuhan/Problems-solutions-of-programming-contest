8aeda75aa2c14f1323831783c3a4c06a86659083
Nowshad Ruhani <nowshad.cse@gmail.com>
2022-09-07 00:15:06 +0600

    update
    update

--- 83. Remove Duplicates from Sorted List - Copy.py
+++ 83. Remove Duplicates from Sorted List - Copy.py
@@ -1,67 +1,25 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "464be4f6",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Definition for singly-linked list.\n",
-    "# class ListNode(object):\n",
-    "#     def __init__(self, val=0, next=None):\n",
-    "#         self.val = val\n",
-    "#         self.next = next\n",
-    "class Solution(object):\n",
-    "    def deleteDuplicates(self, head):\n",
-    "        \"\"\"\n",
-    "        :type head: ListNode\n",
-    "        :rtype: ListNode\n",
-    "        \"\"\"\n",
-    "        # print(head)\n",
-    "        if head == None:\n",
-    "            return None\n",
-    "        \n",
-    "        # define head list to first list, it just define the address of value. that means here first.val is 1 means it showing head.val, which is 1.\n",
-    "        first = head\n",
-    "        while first.next:\n",
-    "            if first.val == first.next.val:\n",
-    "                first.next = first.next.next\n",
-    "            else:\n",
-    "                first = first.next\n",
-    "            \n",
-    "        # print(first)\n",
-    "        return head"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "d0a33548",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.8"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # print(head)
+        if head == None:
+            return None
+        
+        # define head list to first list, it just define the address of value. that means here first.val is 1 means it showing head.val, which is 1.
+        first = head
+        while first.next:
+            if first.val == first.next.val:
+                first.next = first.next.next
+            else:
+                first = first.next
+            
+        # print(first)
+        return head
