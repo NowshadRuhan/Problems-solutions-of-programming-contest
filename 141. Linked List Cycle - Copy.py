65c5a4efff241ad71a6a2347053206026722407f
Nowshad Ruhani <nowshad.cse@gmail.com>
2022-09-09 03:18:02 +0600

    Find LinkList cycle

---
+++ 141. Linked List Cycle - Copy.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        #print(head)
+        while head:
+            if head.val == "No":
+                return True
+            
+            head.val = "No"
+            head = head.next
+            # print(head)
+            
+        return False
