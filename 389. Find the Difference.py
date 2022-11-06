4743f7dca1ab538e08ec6fb8caeb0df7a0f2969d
Nowshad Ruhani <nowshad.cse@gmail.com>
2022-11-06 15:04:11 +0600

    Find the difference between two string, it will a char, in two time, or a new char, we need to find the difference between two strings, LeetCode easy problem with well explain solution

---
+++ 389. Find the Difference.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        for i in t:
+            if i not in s or s.count(i) != t.count(i):
+                return i
